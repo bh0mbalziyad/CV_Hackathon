558c74f4a8211d26884b273a1e2fce6d9d7a1264
Ziyad Bhombal <zeebhombal@gmail.com>
2022-10-17 22:26:17 +0530

    tweak: setup for sane person use

---
+++ README.md
@@ -0,0 +1,12 @@
+# Steps to get this working
+
+Remove pycache
+install deps
+
+`pip3 install -r requirements`
+
+
+```python
+python -m nltk.download punkt
+python -m nltk.download stopwords
+```
--- app.py
+++ app.py
@@ -7,21 +7,24 @@ import os
 from werkzeug.utils import secure_filename
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
-import sys, fitz
+import sys
+import fitz
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import CountVectorizer
 
-def allowedExtension(filename):
-    return '.' in filename and filename.rsplit('.',1)[1].lower() in ['docx','pdf']
 
-jdextractorObj = pickle.load(open("jdExtraction.pkl","rb"))
-resumeExtractionObj = pickle.load(open("resumeExtraction.pkl","rb"))
+def allowedExtension(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ['docx', 'pdf']
+
+
+jdextractorObj = pickle.load(open("jdExtraction.pkl", "rb"))
+resumeExtractionObj = pickle.load(open("resumeExtraction.pkl", "rb"))
 app = Flask(__name__)
 UPLOAD_FOLDER = 'static/JD'
-app.config['UPLOAD_FOLDER']=UPLOAD_FOLDER
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 app.secret_key = "Resume_screening"
-app.config['MONGO_URI'] = "mongodb+srv://userpratik:RyuMongo$99@resumeanalytics.cf3os.mongodb.net/myFirstDatabase?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE"
+app.config['MONGO_URI'] = "mongodb+srv://dbUser:dbpassword@cluster0.v3meaob.mongodb.net/test"
 mongo = PyMongo(app)
 dbJD = mongo.db.JD
 dbResume = mongo.db.dbResume
@@ -31,61 +34,75 @@ dbResume = mongo.db.dbResume
 def index():
     return render_template("index.html")
 
+
 @app.route("/uploadJD", methods=['POST'])
 def uploadJD():
     try:
         file = request.files['jd']
         filename = secure_filename(file.filename)
         if file and allowedExtension(file.filename):
-             file.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
-             fetchedData=jdextractorObj.extractorData("static/JD/"+filename,file.filename.rsplit('.',1)[1].lower())
-             result = None
-             result1 = dbJD.insert_one({"Skills":list(fetchedData[0]),"Education":fetchedData[1],"JD_Data":fetchedData[2]}).inserted_id
-             if result1 == None:
-                return render_template("StartFind.html",successMsg="Problem in Data Storage")
-             else:
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            fetchedData = jdextractorObj.extractorData(
+                "static/JD/"+filename, file.filename.rsplit('.', 1)[1].lower())
+            result = None
+            result1 = dbJD.insert_one({"Skills": list(
+                fetchedData[0]), "Education": fetchedData[1], "JD_Data": fetchedData[2]}).inserted_id
+            if result1 == None:
+                return render_template("StartFind.html", successMsg="Problem in Data Storage")
+            else:
                 session['jd_id'] = str(result1)
-                return render_template("StartFind.html",successMsg="Job Description Uploaded!!")
-    except:
-        print("Exception Occured")
-def matcher(job_desc,resume_text):
-    text=[resume_text,job_desc]
-    cv=CountVectorizer()
-    count_matrix=cv.fit_transform(text)
-    matchper=cosine_similarity(count_matrix)[0][1] * 100
-    return round(matchper,3)
+                return render_template("StartFind.html", successMsg="Job Description Uploaded!!")
+    except Exception as e:
+        print(e)
+
+
+def matcher(job_desc, resume_text):
+    text = [resume_text, job_desc]
+    cv = CountVectorizer()
+    count_matrix = cv.fit_transform(text)
+    matchper = cosine_similarity(count_matrix)[0][1] * 100
+    return round(matchper, 3)
+
 
 @app.route("/showCandidates")
 def showCandidates():
-    TopEmployeers=None
-    TopEmployeers=None
-    TopEmployeers=dbResume.find({"Total_Percentage":{"$gt":20},"JD_ID":{"$eq":session['jd_id']}},{"Name":1,"Mobile_no":1,"Email":1,"Skills":1,"Skills_percentage":1,"Education":1,"Total_Percentage":1}).sort([("Total_Percentage",-1)])
+    TopEmployeers = None
+    TopEmployeers = None
+    TopEmployeers = dbResume.find({"Total_Percentage": {"$gt": 20}, "JD_ID": {"$eq": session['jd_id']}}, {
+                                  "Name": 1, "Mobile_no": 1, "Email": 1, "Skills": 1, "Skills_percentage": 1, "Education": 1, "Total_Percentage": 1}).sort([("Total_Percentage", -1)])
     if TopEmployeers == None:
-        return render_template("Show_top_matching.html",successMsg="No Candidate found")
+        return render_template("Show_top_matching.html", successMsg="No Candidate found")
     else:
-        selectedResumes={}
+        selectedResumes = {}
         cnt = 0
         for i in TopEmployeers:
-            selectedResumes[cnt] = {"Name":i['Name'],"Email":i['Email'],"Mobile_no":i['Mobile_no'],"Skills":i['Skills'],"Skills_percentage":i['Skills_percentage'],"Education":i['Education'],"Total_Percentage":i['Total_Percentage']}
+            selectedResumes[cnt] = {"Name": i['Name'], "Email": i['Email'], "Mobile_no": i['Mobile_no'], "Skills": i['Skills'],
+                                    "Skills_percentage": i['Skills_percentage'], "Education": i['Education'], "Total_Percentage": i['Total_Percentage']}
             cnt += 1
-        return render_template("Show_top_matching.html",len = len(selectedResumes), data = selectedResumes,successMsg="Shortlisted Candidates")
+        return render_template("Show_top_matching.html", len=len(selectedResumes), data=selectedResumes, successMsg="Shortlisted Candidates")
+
 
 @app.route("/scanResume")
 def scanResume():
-    se=dbJD.find_one({"_id":ObjectId(session['jd_id'])},{"Skills":1,"Education":1,"JD_Data":1})
+    se = dbJD.find_one({"_id": ObjectId(session['jd_id'])}, {
+                       "Skills": 1, "Education": 1, "JD_Data": 1})
     entries = os.listdir('Resumes/')
     for entry in entries:
         try:
-            data = resumeExtractionObj.extractorData('Resumes/'+entry,"pdf")
-            da = round((len(list(set(se['Skills']).intersection(data[3])))/len(list(se['Skills'])))*100,3)
-            skills_percentage = round((da * 60 / 100),3)
-            resume_percentage = matcher(se['JD_Data'],data[5])
-            total = round((skills_percentage + (resume_percentage * 40) / 100),3)
+            data = resumeExtractionObj.extractorData('Resumes/'+entry, "pdf")
+            da = round((len(list(set(se['Skills']).intersection(
+                data[3])))/len(list(se['Skills'])))*100, 3)
+            skills_percentage = round((da * 60 / 100), 3)
+            resume_percentage = matcher(se['JD_Data'], data[5])
+            total = round(
+                (skills_percentage + (resume_percentage * 40) / 100), 3)
             result = None
-            result = dbResume.insert_one({"Name":data[0],"Mobile_no":data[1],"Email":data[2],"Skills":list(data[3]),"Skills_percentage":da,"Education":data[4],"JD_Percentage":resume_percentage,"Total_Percentage":total,"JD_ID":session['jd_id']}).inserted_id
+            result = dbResume.insert_one({"Name": data[0], "Mobile_no": data[1], "Email": data[2], "Skills": list(
+                data[3]), "Skills_percentage": da, "Education": data[4], "JD_Percentage": resume_percentage, "Total_Percentage": total, "JD_ID": session['jd_id']}).inserted_id
         except:
             continue
     return redirect(url_for("showCandidates"))
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     app.run(debug=True)
